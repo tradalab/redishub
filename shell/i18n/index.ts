1b0e6d3d6199d5aadb0010c697f7ddacc29d99ab
trungnq2710 <trungnq2710@gmail.com>
2026-01-28 16:10:43 +0700

    feat(i18n): add ja

--- shell/i18n/index.ts
+++ shell/i18n/index.ts
@@ -4,11 +4,13 @@ import i18n from "i18next"
 import { initReactI18next } from "react-i18next"
 
 import en from "./en.json"
+import ja from "./ja.json"
 
 if (!i18n.isInitialized) {
   i18n.use(initReactI18next).init({
     resources: {
       en: { translation: en },
+      ja: { translation: ja },
     },
     lng: "en",
     fallbackLng: "en",
